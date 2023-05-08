dc1a5f925eae86e16a77eacc5b1d179e7d65e414
neisonp <nelsonponte37@gmail.com>
2023-05-08 12:47:11 +0000

    added login route

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -52,4 +52,38 @@ module.exports = class UserController {
             res.status(500).json({ message: error });
         }
     }
+
+    static async login(req, res) {
+
+        const { email, password } = req.body;
+
+        const fields = [
+            { name: email, message: 'The email is required!' },
+            { name: password, message: 'The password is required!' }
+        ];
+
+        for (const field of fields) {
+            if (!field.name) {
+                return res.status(422).json({ message: field.message });
+            }
+        }
+
+        const user = await User.findOne({ email: email });
+
+        if (!user) {
+            return res.status(422).json({
+                message: 'There is no user registered with that email'
+            })
+        }
+
+        const checkPassword = await bcrypt.compareSync(password, user.password);
+
+        if (!checkPassword) {
+            return res.status(422).json({
+                message: 'Invalid password'
+            });
+        }
+
+        await createuserToken(user, req, res);
+    }
 }
