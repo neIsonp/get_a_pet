00bb319c7735f33e0c55d46975c4b6b8b6c31d81
neIsonp <nelsonponte37@gmail.com>
2023-05-13 13:53:08 +0000

    added all pets

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -66,4 +66,14 @@ module.exports = class PetController {
         }
 
     }
+
+    static async getAll(req, res) {
+        const pets = await Pet.find().sort('-createAt');
+
+        res.status(200).json({
+            pets: pets
+        })
+    }
+
+
 }
