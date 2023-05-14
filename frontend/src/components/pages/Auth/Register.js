b9f13c57789e60a451a4262d810eb70ed216d4a8
neIsonp <nelsonponte37@gmail.com>
2023-05-14 17:23:55 +0000

    created hook useAuth

--- frontend/src/components/pages/Auth/Register.js
+++ frontend/src/components/pages/Auth/Register.js
@@ -1,15 +1,26 @@
 import Input from "../../form/input";
+import { useState } from "react";
 import styles from '../../form/Form.module.css';
 import { Link } from "react-router-dom";
 
 function Register() {
 
-    function handleChange(e) { }
+    const [user, setUser] = useState({});
+
+    function handleChange(e) {
+        setUser({ ...user, [e.target.name]: e.target.value });
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        console.log(user);
+    }
 
     return (
         <section className={styles.form_container}>
             <h1>Register</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <Input text="Name" type="text" name="name" placeholder="Insert your name" handleOnChange={handleChange} />
                 <Input text="Phone" type="text" name="phone" placeholder="Insert your phone number" handleOnChange={handleChange} />
                 <Input text="Email" type="email" name="email" placeholder="Insert your email" handleOnChange={handleChange} />
