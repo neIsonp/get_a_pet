512cf4051101d913b856770d9e587f2067b675d3
neIsonp <nelsonponte37@gmail.com>
2023-05-15 13:58:25 +0000

    created login page

--- frontend/src/components/pages/Auth/Login.js
+++ frontend/src/components/pages/Auth/Login.js
@@ -1,7 +1,36 @@
+import { useState, useContext } from "react";
+import Input from "../../form/input";
+import styles from "../../form/Form.module.css";
+
+import { Context } from "../../../context/UserContext";
+import { Link } from "react-router-dom";
+
 export default function Login() {
-    return (
-        <section>
-            <h1>Login</h1>
-        </section>
-    );
+  function handleChange(e) {}
+
+  return (
+    <section className={styles.form_container}>
+      <h1>Login</h1>
+      <form>
+        <Input
+          text="E-mail"
+          type="email"
+          name="email"
+          placeholder="Insert your email"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Password"
+          type="password"
+          name="password"
+          placeholder="Insert your password"
+          handleOnChange={handleChange}
+        />
+        <input type="submit" value="Login" />
+      </form>
+      <p>
+        Don't have an account? <Link to="/register">Click here</Link>
+      </p>
+    </section>
+  );
 }
--- frontend/src/components/pages/Auth/Register.js
+++ frontend/src/components/pages/Auth/Register.js
@@ -1,37 +1,69 @@
 import Input from "../../form/input";
 import { useState, useContext } from "react";
-import styles from '../../form/Form.module.css';
+import styles from "../../form/Form.module.css";
 import { Link } from "react-router-dom";
-import { Context } from '../../../context/UserContext'
+import { Context } from "../../../context/UserContext";
 
 function Register() {
+  const [user, setUser] = useState({});
+  const { register } = useContext(Context);
 
-    const [user, setUser] = useState({});
-    const { register } = useContext(Context);
+  function handleChange(e) {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  }
 
-    function handleChange(e) {
-        setUser({ ...user, [e.target.name]: e.target.value });
-    }
+  function handleSubmit(e) {
+    e.preventDefault();
 
-    function handleSubmit(e) {
-        e.preventDefault();
+    register(user);
+  }
 
-        register(user);
-    }
+  return (
+    <section className={styles.form_container}>
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <Input
+          text="Name"
+          type="text"
+          name="name"
+          placeholder="Insert your name"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Phone"
+          type="text"
+          name="phone"
+          placeholder="Insert your phone number"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Email"
+          type="email"
+          name="email"
+          placeholder="Insert your email"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Password"
+          type="password"
+          name="password"
+          placeholder="Insert your password"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Confirm Password"
+          type="password"
+          name="confirmpassword"
+          placeholder="Confirm password"
+          handleOnChange={handleChange}
+        />
+        <input type="submit" value="Register" />
+      </form>
 
-    return (
-        <section className={styles.form_container}>
-            <h1>Register</h1>
-            <form onSubmit={handleSubmit}>
-                <Input text="Name" type="text" name="name" placeholder="Insert your name" handleOnChange={handleChange} />
-                <Input text="Phone" type="text" name="phone" placeholder="Insert your phone number" handleOnChange={handleChange} />
-                <Input text="Email" type="email" name="email" placeholder="Insert your email" handleOnChange={handleChange} />
-                <Input text="Password" type="password" name="password" placeholder="Insert your password" handleOnChange={handleChange} />
-                <Input text="Confirm Password" type="password" name="confirmpassword" placeholder="Confirm password" handleOnChange={handleChange} />
-                <input type="submit" value="Register" />
-            </form>
-            <p>You already have an account ? <Link to="/login">CLick here</Link></p>
-        </section>
-    );
+      <p>
+        You already have an account ? <Link to="/login">CLick here</Link>
+      </p>
+    </section>
+  );
 }
 export default Register;
